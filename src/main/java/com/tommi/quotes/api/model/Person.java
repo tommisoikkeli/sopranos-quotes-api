9c78eda424b4ce3f36e35dfcbe94922f5af2434e
Tommi Soikkeli <soikkelitommi@gmail.com>
2019-02-12 18:09:28 +0200

    removed unused imports

--- src/main/java/com/tommi/quotes/api/model/Person.java
+++ src/main/java/com/tommi/quotes/api/model/Person.java
@@ -1,8 +1,6 @@
 package com.tommi.quotes.api.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
